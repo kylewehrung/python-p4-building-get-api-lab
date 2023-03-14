b1cc674a8456396d56c95d58cc65a9c6562a5456
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-13 23:10:05 -0600

    pretty coooool

--- server/seed.py
+++ server/seed.py
@@ -7,7 +7,7 @@ from faker import Faker
 from app import app
 from models import db, Bakery, BakedGood
 
-db.init_app(app)
+# db.init_app(app)
 
 fake = Faker()
 
@@ -49,3 +49,42 @@ with app.app_context():
     most_expensive_baked_good.price = 100
     db.session.add(most_expensive_baked_good)
     db.session.commit()
+
+#!/usr/bin/env python3
+
+# from flask import Flask, make_response, jsonify
+# from flask_migrate import Migrate
+
+# from models import db, Bakery, BakedGood
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Bakery GET API</h1>'
+
+# @app.route('/bakeries')
+# def bakeries():
+#     return ''
+
+# @app.route('/bakeries/<int:id>')
+# def bakery_by_id(id):
+#     return ''
+
+# @app.route('/baked_goods/by_price')
+# def baked_goods_by_price():
+#     return ''
+
+# @app.route('/baked_goods/most_expensive')
+# def most_expensive_baked_good():
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
